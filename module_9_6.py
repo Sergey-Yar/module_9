6588bf696fdadcc006cdcff3c30455d0acfb5669
Сергей <ssergant@mail.ru>
2024-11-06 23:53:26 +0300

    Добавил i в 5 строчку для более красивого вывода

--- module_9_6.py
+++ module_9_6.py
@@ -2,7 +2,7 @@
 def all_variants(text):
     var = len(text)
     for i in range(var):
-        for j in range(var):
+        for j in range(i, var):
             yield text[i:j + 1]
 
 a = all_variants("abc")
