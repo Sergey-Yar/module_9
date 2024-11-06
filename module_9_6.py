2eaa29aa5280949756c3e4c682801820d0797d55
Сергей <ssergant@mail.ru>
2024-11-06 21:22:08 +0300

    Генераторы

---
+++ module_9_6.py
@@ -0,0 +1,10 @@
+#Генераторы
+def all_variants(text):
+    var = len(text)
+    for i in range(var):
+        for j in range(var):
+            yield text[i:j + 1]
+
+a = all_variants("abc")
+for i in a:
+    print(i)
